4759d968e0eef5d4c4e61d64ddd670cb9e240992
underscoreanuj <underscoreanuj@gmail.com>
2020-01-29 15:13:00 +0530

    inception

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,23 @@
-require(`dotenv`).config({
-  path: `.env`,
-})
+require(`dotenv`).config({path: `.env`});
 
 module.exports = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Anuj Singh - Portfolio`
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
       // See the theme's README for all available options
-      options: {},
-    },
-    {
+      options: {}
+    }, {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
+    }, {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
@@ -35,17 +31,16 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
+            type: `image/png`
+          }, {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
-}
+    `gatsby-plugin-netlify`
+  ]
+};
